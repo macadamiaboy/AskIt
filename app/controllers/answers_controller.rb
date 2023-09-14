3000da1ef7030aa47310b3a5445d088a865ab536
macadamiaboy <elknumber1@yandex.com>
2023-09-14 20:37:36 +0300

    Answers edition added

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,20 @@
 class AnswersController < ApplicationController
   before_action :set_question!
+  before_action :set_answer!, except: :create
+
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      redirect_to questions_path(@question)
+    else
+      render :edit
+    end
+  end
 
   def destroy
-    answer = @question.answers.find params[:id]
-    answer.delete
+    @answer.delete
     redirect_to questions_path(@question)
   end
 
@@ -25,5 +36,9 @@ class AnswersController < ApplicationController
 
   def set_question!
     @question = Question.find params[:question_id]
+  end
+
+  def set_answer!
+    @answer = @question.answers.find params[:id]
   end
 end
